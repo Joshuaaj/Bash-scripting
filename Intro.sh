91248ef1bdfbd7ba239b325414fcffed0ffffd7c
Joshuaaj <joshuajoz123@gmail.com>
2023-11-23 11:27:26 +0000

    added

---
+++ Intro.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Hello World"
+
+echo "execute tis file by using 'chmod +x Intro.sh' chmod for setting file permission and +x flag adds execute permission to the file"
+
+echo "Then see the output using './Intro.sh'"
+
+echo "Do this for all files to see the output in the terminal"
